bfd21a831aee7d3f0cb432ecbd76c4a65125f10f
boomer <90579797+boomerchan@users.noreply.github.com>
2023-10-17 09:40:19 -0500

    procrastinating

--- serve_local.py
+++ serve_local.py
@@ -1,10 +1,15 @@
 import http.server
 import socketserver
+import argparse
 
-PORT = 8000
+parser = argparse.ArgumentParser(description='HTTP Server')
+parser.add_argument('--port', type=int, help='specify the port number', default=7865)
+args = parser.parse_args()
+
+PORT = args.port
 
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Serving at port", PORT)
+    print(f"Serving at http://localhost:{PORT}")
     httpd.serve_forever()
